0968be343821c2cf8fab2cd764ad10b202e031c2
Bolotnikova Elena <lenamysterytour@gmail.com>
2023-06-06 17:39:38 +0300

    tiny naming fix

--- src/test/java/JavaHover.java
+++ src/test/java/JavaHover.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.*;
 
-public class javaHover {
+public class JavaHover {
 
     @BeforeAll
     static void beforeAll() {
